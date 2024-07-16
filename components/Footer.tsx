ecc81588476fcc9397ec5bea2cbedca07e552849
Tseko Tsolov <tseko.tsolov@mentormate.com>
2024-07-16 18:34:27 +0300

    added 404 and loading pages

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import logo from "@/assets/images/logo.png";
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <footer className="bg-gray-200 py-4 mt-52">
+    <footer className="bg-gray-200 py-4">
       <div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-4">
         <div className="mb-4 md:mb-0">
           <Image src={logo} alt="Logo" className="h-8 w-auto" />
