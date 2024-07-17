7210c6c90887a29ac63e8f928e3824a633ab3141
Tseko Tsolov <tseko.tsolov@mentormate.com>
2024-07-17 16:53:12 +0300

    added AuthProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import "@/assets/styles/globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import AuthProvider from "@/components/AuthProvider";
 
 interface MainLayoutProps {
   children: ReactNode;
@@ -16,13 +17,15 @@ export const metadata = {
 
 const MainLayout = ({ children }: MainLayoutProps) => {
   return (
-    <html lang="en">
-      <body>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
   );
 };
 
