22ac09160346fcb3e7cd991bcd59f2a0519e1f22
Tseko Tsolov <tseko.tsolov@mentormate.com>
2024-07-19 15:19:41 +0300

    added search functionality

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -1,26 +1,34 @@
 import PropertyCard from "@/components/PropertyCard";
+import PropertySearchForm from "@/components/PropertySearchForm";
 import { Property } from "@/types/property.type";
 import { fetchProperties } from "@/utils/requests";
 
 const PropertiesPage = async () => {
   const properties = await fetchProperties();
   return (
-    <section className="px-4 py-6">
-      <div className="container-xl lg:container m-auto">
-        <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">
-          Properties
-        </h2>
-        {properties?.length === 0 ? (
-          <div>No properties found</div>
-        ) : (
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {properties?.map((property: Property) => (
-              <PropertyCard key={property._id} property={property} />
-            ))}
-          </div>
-        )}
-      </div>
-    </section>
+    <>
+      <section className="bg-blue-700 py-4">
+        <div className="max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8">
+          <PropertySearchForm />
+        </div>
+      </section>
+      <section className="px-4 py-6">
+        <div className="container-xl lg:container m-auto">
+          <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">
+            Properties
+          </h2>
+          {properties?.length === 0 ? (
+            <div>No properties found</div>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {properties?.map((property: Property) => (
+                <PropertyCard key={property._id} property={property} />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+    </>
   );
 };
 
